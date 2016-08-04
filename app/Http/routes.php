2e6183c3fe2dff265e553c7ab7824eba49d5ee98
vagrant <vagrant@homestead>
2016-08-04 03:44:15 +0000

    env

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,6 +8,9 @@
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
+
+git remote add origin https://github.com/Josiejosi/wallet.git
+git push -u origin master
 |
 */
 
